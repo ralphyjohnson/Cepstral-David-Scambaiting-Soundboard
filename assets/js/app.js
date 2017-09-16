5e2dfb485dafab72dad6a08b774de0e1ba3b18a0
ralphyjohnson <31601720+ralphyjohnson@users.noreply.github.com>
2017-09-16 13:42:53 -0400

    speech testing

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,38 @@
 let html = '';
 
-// print function
+// Text to speech on click
+function say(msg) {
+    with(speechSynthesis) {
+        var s = new(SpeechSynthesisUtterance)(msg);
+        s.voice = getVoices[0];
+        speak(s);
+    }
+}
+var tag = document.querySelectorAll('button')
+for (i = 0; i < tag.length; i++) {
+    tag[i].addEventListener('click', function(e) {
+        say(e.target.value);
+    });
+}
+
+// print function for #soundboard
 function print(message) {
   var outputDiv = document.getElementById('soundboard');
   outputDiv.innerHTML = message;
 }
 
-// List all phrases fron JSON
+// Open soundboard container
+html += '<div class="soundboard-container">';
+
+// List all phrases from JSON
 html += '<ul>';
 for ( var phrase in phrases ) {
-	html += '<li>' + phrases[phrase].say + '</li>';
+	html += '<li><button>' + phrases[phrase].say + '</button></li>';
 }
 html += '</ul>';
 
-// Print html on the page
+// Close soundboard container
+html += '</div> <!-- .soundboard-container -->';
+
+// Print soundboard html on the page
 print(html);
---
+++ assets/js/newPhrases.js
@@ -0,0 +1,47 @@
+var phrases = [
+  {
+    category : 'Introduction',
+    greeting : [
+      'Hi',
+      'Hey',
+      'Hello',
+      'Hello. How Are You'
+    ],
+    stalling : [
+      'Can you hear me',
+      'I\'m sorry. I can\'t hear you',
+      'Can you please speak louder',
+      'Can you speak a little louder',
+      'It\'s hard for me to hear you',
+      'Speak up please. It\'s hard for me to hear'
+    ],
+    stallReply : [
+      'Yes. I can hear you now',
+      '',
+      '',
+      '',
+      ''
+    ],
+  },
+  {
+    category : 'Explanation'
+  },
+  {
+    category : 'Questions'
+  },
+  {
+    category : 'Responses'
+  },
+  {
+    category : 'Identity'
+  },
+  {
+    category : 'Information'
+  },
+  {
+    category : 'Trollulz'
+  },
+  {
+    category : 'Swearing'
+  }
+];
