4dd573fe8f2d7ab4bf585158960b8ff4af8dc197
ralphyjohnson <31601720+ralphyjohnson@users.noreply.github.com>
2017-09-16 16:22:29 -0400

    testing cepstral david

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,5 @@
 let html = '';
 
-// Text to speech on click
-function say(msg) {
-    with(speechSynthesis) {
-        var s = new(SpeechSynthesisUtterance)(msg);
-        s.voice = getVoices[0];
-        speak(s);
-    }
-}
-var tag = document.querySelectorAll('button')
-for (i = 0; i < tag.length; i++) {
-    tag[i].addEventListener('click', function(e) {
-        say(e.target.value);
-    });
-}
-
 // print function for #soundboard
 function print(message) {
   var outputDiv = document.getElementById('soundboard');
@@ -27,7 +12,7 @@ html += '<div class="soundboard-container">';
 // List all phrases from JSON
 html += '<ul>';
 for ( var phrase in phrases ) {
-	html += '<li><button>' + phrases[phrase].say + '</button></li>';
+	html += '<li><input type="button" value="' + phrases[phrase].say + '"></input></li>';
 }
 html += '</ul>';
 
@@ -36,3 +21,20 @@ html += '</div> <!-- .soundboard-container -->';
 
 // Print soundboard html on the page
 print(html);
+
+// Text to speech on click
+function say( msg ) {
+    with(speechSynthesis) {
+        var s = new(SpeechSynthesisUtterance)(msg);
+        s.voice = getVoices()[0];
+        speak(s);
+    }
+}
+
+// what does the TTS say
+var tag = document.querySelectorAll('input');
+for ( i = 0; i < tag.length; i++ ) {
+    tag[i].addEventListener('click', function(e) {
+        say(e.target.value);
+    });
+}
